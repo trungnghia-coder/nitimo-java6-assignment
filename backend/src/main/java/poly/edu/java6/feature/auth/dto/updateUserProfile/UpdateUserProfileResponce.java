f38613aaa95087c0d0cdbd15d2b50ab56fead1b1
Nguyễn Trung Nghĩa <nguyentrunghia0907@gmail.com>
2025-11-28 23:42:07 +0700

    fix: remove redundancy

---
+++ backend/src/main/java/poly/edu/java6/feature/auth/dto/updateUserProfile/UpdateUserProfileResponce.java
@@ -0,0 +1,2 @@
+package poly.edu.java6.feature.auth.dto.updateUserProfile;public class UpdateUserProfileResponce {
+}
---
+++ backend/src/main/java/poly/edu/java6/feature/auth/service/CustomUserDetails.java
@@ -0,0 +1,2 @@
+package poly.edu.java6.feature.auth.service;public class CustomUserDetails {
+}
